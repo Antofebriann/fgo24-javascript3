41eb1215eff820664d83e895cfbd7b6af3658d2c
Antofebriann <113361419+Antofebriann@users.noreply.github.com>
2025-04-21 15:31:25 +0700

    Add files via upload

--- minitask4.js
+++ minitask4.js
@@ -3,8 +3,13 @@ function isPalindrome(word) {
     const reversed = word.split("").reverse().join("");
     return word === reversed;
   }
+function reverseWord(word) {
+    return word.split("").reverse().join("");
+}
+
+
   const input = "motor";
 
   console.log(`Kata: ${input}`);
   console.log(`Palindrome? ${isPalindrome(input) ? "Ya" : "Tidak"}`);
-  
+  console.log(`Reverse: ${reverseWord(input)}`)
